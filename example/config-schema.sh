3c2cacff9b8054058cd9c8915fdbdcfe3ec500b5
Shane Myrick <shane@apollographql.com>
2024-10-02 13:25:56 -0700

    feat: add example folder

--- example/config-schema.sh
+++ example/config-schema.sh
@@ -1,1 +1,1 @@
-docker run ghcr.io/apollosolutions/persisted-query-to-rest:v0.3.2 config-schema > config-schema.json
+docker run ghcr.io/apollosolutions/persisted-query-to-rest:latest config-schema > config-schema.json
--- example/start.sh
+++ example/start.sh
@@ -1,5 +1,4 @@
 docker run \
-  -p 8080:8080 \
+  -it -p 8080:8080 \
   --mount "type=bind,source=./config.yml,target=/app/config.yml" \
-  --name persisted-query-to-rest \
-  ghcr.io/apollosolutions/persisted-query-to-rest:v0.3.2 --config /app/config.yml
+  ghcr.io/apollosolutions/persisted-query-to-rest:latest --config /app/config.yml
